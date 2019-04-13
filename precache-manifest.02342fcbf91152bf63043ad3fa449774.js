4c8a8c94b9ccb48a79c63b0c87f96cfd08ae3121
JMurphy <murph_iraq@yahoo.com>
2019-04-13 12:12:01 -0400

    Updates

---
+++ precache-manifest.02342fcbf91152bf63043ad3fa449774.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1128ffc203b63ba3ad9d",
+    "url": "/Jmurphy72383/react-clicky-game/static/js/runtime~main.1128ffc2.js"
+  },
+  {
+    "revision": "f371590fe881b1fc3917",
+    "url": "/Jmurphy72383/react-clicky-game/static/js/main.f371590f.chunk.js"
+  },
+  {
+    "revision": "5073f3b9b0d52c703464",
+    "url": "/Jmurphy72383/react-clicky-game/static/js/1.5073f3b9.chunk.js"
+  },
+  {
+    "revision": "f371590fe881b1fc3917",
+    "url": "/Jmurphy72383/react-clicky-game/static/css/main.15098afa.chunk.css"
+  },
+  {
+    "revision": "d4b46b61255fb564e5e40c4831c029b4",
+    "url": "/Jmurphy72383/react-clicky-game/index.html"
+  }
+];
